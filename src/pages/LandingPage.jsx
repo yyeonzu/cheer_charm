3c566ede282b68f894513bdbd49069c59506a062
yyeonzu <tooma22@naver.com>
2023-01-14 00:47:58 +0900

    feat: 랜딩 페이지(로그인X) UI 퍼블리싱

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import LoginLanding from '../components/Landing/LoginLanding';
+import LogoutLanding from '../components/Landing/LogoutLanding';
+
+const LandingPage = () => {
+  // 이것도 토큰값을 통해서 return 받아 옵니다...
+  const [isLogin, setIsLogin] = useState(false);
+
+  return <>{isLogin ? <LoginLanding /> : <LogoutLanding />}</>;
+};
+
+export default LandingPage;
