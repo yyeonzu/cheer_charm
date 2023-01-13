2aa7c23bfdd36e1be53b3748739c4b1bd0987f1a
yyeonzu <tooma22@naver.com>
2023-01-14 00:47:58 +0900

    랜딩 페이지(로그인X) UI 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 // 로그인 & 회원가입 페이지
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import SendCheerPage from './pages/SendCheerPage';
 
 function App() {
   return (
@@ -13,6 +15,12 @@ function App() {
           {/* 로그인 & 회원가입 */}
           <Route exact path='/login' element={<LoginPage />} />
           <Route exact path='/register' element={<RegisterPage />} />
+
+          {/* 랜딩페이지 (메인페이지) */}
+          <Route exact path='/main' element={<LandingPage />} />
+
+          {/* 응원 전송 페이지 */}
+          <Route exact path='/sendcheer' element={<SendCheerPage />} />
         </Routes>
       </BrowserRouter>
     </>
