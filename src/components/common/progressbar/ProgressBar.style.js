f48ace85d7c12d053c03c89212c615d6a21d57c3
서진 <dltjwls0529@gmail.com>
2023-01-20 19:54:22 +0900

    fix: 프로그레스 바 옵션 설정

--- src/components/common/progressbar/ProgressBar.style.js
+++ src/components/common/progressbar/ProgressBar.style.js
@@ -1,48 +1,58 @@
-import styled from 'styled-components';
-import clovericon from '../../../assets/images/Landing/clover2.svg';
+import styled from "styled-components";
+import clovericon from "../../../assets/images/Landing/clover2.svg";
 
 export const Container = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  height: 30px;
-  position: relative;
-  /* border: solid 1px; */
+	width: 100%;
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	height: 30px;
+	position: relative;
+	/* border: solid 1px; */
 `;
 
 export const ProgressContainer = styled.div`
-  /* border: solid 1px; */
-  height: 100%;
-  position: relative;
-  display: flex;
-  align-items: center;
-  overflow: initial;
+	/* border: solid 1px; */
+	height: 100%;
+	position: relative;
+	display: flex;
+	align-items: center;
+	overflow: initial;
 `;
 
 export const CloverProgress = styled.div`
-  width: 134px;
-  height: 12px;
-  border: 0.5px solid #000000;
-  border-radius: 7.5px;
-  display: flex;
-  align-items: center;
-  overflow: hidden;
-  position: relative;
+	width: 134px;
+	height: 12px;
+	border: 1px solid #000;
+	border-radius: 7.5px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	overflow: hidden;
+	position: relative;
+	.inner {
+		width: 132px;
+		height: 10px;
+		border-radius: 7px;
+		display: flex;
+		align-items: center;
+		overflow: hidden;
+		position: relative;
+	}
 `;
 
 export const Progress = styled.div`
-  width: 100%;
-  position: absolute;
-  left: ${(props) => -100 + props.percent}%;
-  top: 0px;
-  height: 100%;
-  background: rgba(110, 190, 124, 0.59);
-  border-radius: 7.5px;
+	width: 100%;
+	position: absolute;
+	left: ${(props) => -100 + props.percent}%;
+	top: 0px;
+	height: 100%;
+	background-color: rgba(110, 190, 124, 0.59);
+	border-radius: 7.5px;
 
-  // 애니메이션 관련, 미완
+	// 애니메이션 관련, 미완
 
-  /* transform-origin: left;
+	/* transform-origin: left;
   animation: scaleX 1s forwards ease-out;
 
   @keyframes scaleX {
@@ -56,14 +66,14 @@ export const Progress = styled.div`
 `;
 
 export const CloverIcon = styled.img.attrs({ src: `${clovericon}` })`
-  position: absolute;
-  left: ${(props) => -7 + props.percent}%;
-  z-index: 5;
-  transform: rotate(-33.39deg);
+	position: absolute;
+	left: ${(props) => -7 + props.percent}%;
+	z-index: 5;
+	transform: rotate(-33.39deg);
 
-  // 애니메이션 관련, 미완
+	// 애니메이션 관련, 미완
 
-  /* transform-origin: left;
+	/* transform-origin: left;
   animation: translateX 1s forwards ease-out;
 
   @keyframes translateX {
@@ -78,11 +88,21 @@ export const CloverIcon = styled.img.attrs({ src: `${clovericon}` })`
 `;
 
 export const FontWrapper = styled.div`
-  /* border: solid 1px; */
-  width: 45px;
-  display: flex;
-  justify-content: flex-end;
-  font-family: 'Galmuri';
-  font-weight: 700;
-  margin-bottom: 3.5px;
+	/* border: solid 1px; */
+	width: 45px;
+	display: flex;
+	justify-content: flex-end;
+	font-family: "Galmuri";
+	font-weight: 700;
+	margin-bottom: 3.5px;
+`;
+
+export const UnderFontWrapper = styled.div`
+	font-family: "Galmuri";
+	font-stretch: condensed;
+	font-weight: 400;
+	font-size: 14px;
+	height: 25px;
+	display: flex;
+	align-items: center;
 `;
