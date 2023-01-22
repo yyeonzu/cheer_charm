de1459be3ac7c6c9383378a0253ce7be3a3c92e8
서진 <dltjwls0529@gmail.com>
2023-01-23 00:25:29 +0900

    fix: 응원 리스트 관련 파일 수정, 결과 페이지 응원 모달 추가

--- src/components/CheerCharm/CheeredCharm.jsx
+++ src/components/CheerCharm/CheeredCharm.jsx
@@ -1,31 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as S from "./CheeredCharm.style";
 import Footer from "../common/Footer";
 import ProgressBar from "../common/progressbar/ProgressBar";
-import CheerList from "./CheerList";
+import CheeredList from "./CheeredList";
+import CheerModal from "./CheerModal";
 import logo from "../../assets/images/CharmPage/charmpagelogo.svg";
 import speechbubble from "../../assets/images/CharmPage/speechbubble.svg";
 import testcharm from "../../assets/images/testcharm.png";
 import { NanoomSquare } from "../../css/Font.js";
 import { MdOutlineFlipCameraAndroid } from "react-icons/md";
 import { FiDownload } from "react-icons/fi";
+import { testlist } from "../../_mock/data2";
+
+import charm1 from "../../assets/images/Charm/charm1.svg";
+import charm2 from "../../assets/images/Charm/charm2.svg";
+import charm3 from "../../assets/images/Charm/charm3.svg";
+import charm4 from "../../assets/images/Charm/charm4.svg";
+import charm5 from "../../assets/images/Charm/charm5.svg";
+import charm6 from "../../assets/images/Charm/charm6.svg";
 
 const CheeredCharm = () => {
-	let total = 10;
-	let done = 10;
-	const nickname = "이름이름";
-	const cheers = [
-		{ name: "일이삼사오육칠팔구십일이", content: "내용~" },
-		{ name: "하이2", content: "내용~" },
-		{ name: "하이3", content: "내용~" },
-		{ name: "하이4", content: "내용~" },
-		{ name: "일이삼사오육칠팔구십일이", content: "내용~" },
-		{ name: "하이6", content: "내용~" },
-		{ name: "하이7", content: "내용~" },
-		{ name: "하이8", content: "내용~" },
-		{ name: "일이삼사오육칠팔구십일이", content: "내용~" },
-		{ name: "하이10", content: "내용~" },
-	];
+	const src = [charm1, charm2, charm3, charm4, charm5, charm6];
+	const nickname = "이이름이름";
+	const currentURL = window.location.href;
+	const [modal, setModal] = useState(false);
+
+	const [cId, setCId] = useState(3);
+	// 부적 개별 조회 api
+
+	const [currentCharm, setCurrentCharm] = useState({});
+	const [total, setTotal] = useState(0);
+	const [done, setDone] = useState(0);
+	useEffect(() => {
+		for (let i = 0; i < testlist.length; i++) {
+			if (testlist[i].id === cId) {
+				setCurrentCharm(testlist[i]);
+			}
+		}
+		setTotal(currentCharm.total_cheer);
+		setDone(currentCharm.cur_cheer);
+		console.log(currentCharm, total, done);
+	});
 	return (
 		<>
 			<S.Container>
@@ -33,7 +48,7 @@ const CheeredCharm = () => {
 					<S.LogoImg src={logo} />
 				</S.LogoContainer>
 				<S.CharmContainer>
-					<S.CharmImg src={testcharm} />
+					<S.CharmImg src={src[currentCharm.image]} />
 				</S.CharmContainer>
 				<ProgressBar total={total} done={done} isRight={false} />
 				<S.BubbleContainer>
@@ -63,10 +78,15 @@ const CheeredCharm = () => {
 					</div>
 				</S.CheerTitleContainer>
 				<S.CheerContainer>
-					<div className="inner">{cheers && <CheerList cheers={cheers} />}</div>
+					<div className="inner">
+						<CheeredList cId={cId} setModal={setModal} />
+					</div>
 				</S.CheerContainer>
 				<Footer />
 			</S.Container>
+			{modal ? (
+				<CheerModal isModalOpen={modal} closer={() => setModal(false)} />
+			) : null}
 		</>
 	);
 };
