05bb327576891db4d556a48cf4401e4691588d1a
서진 <dltjwls0529@gmail.com>
2023-01-22 23:25:22 +0900

    chore: mock 데이터 추가

---
+++ src/_mock/data2.js
@@ -0,0 +1,68 @@
+export const testlist = [
+	{
+		id: 1,
+		title: "부적1",
+		user: 1,
+		content: "부적1내용입니다. 내용입니다.",
+		image: 1,
+		total_cheer: 5,
+		cur_cheer: 4,
+		is_created: false,
+		created_at: "2023-01-13T10:34:01.382686+09:00",
+		deleted_at: null,
+		cheer: [
+			{
+				id: 1,
+				charm: 1,
+				nickname: "익명1(부적1)",
+				content: "부적1의응원입니다1.",
+			},
+		],
+	},
+	{
+		id: 2,
+		title: "부적2이름",
+		user: 1,
+		content: "부적2내용입니다. 내용입니다.",
+		image: 2,
+		total_cheer: 10,
+		cur_cheer: 2,
+		is_created: false,
+		created_at: "2023-01-13T10:34:25.113108+09:00",
+		deleted_at: null,
+		cheer: [
+			{
+				id: 1,
+				charm: 2,
+				nickname: "부적2의응원1",
+				content: "부적2의응원1입니다1.",
+			},
+			{
+				id: 2,
+				charm: 2,
+				nickname: "부적2의응원2",
+				content: "부적2의응원2입니다2.",
+			},
+		],
+	},
+	{
+		id: 3,
+		title: "부적3이름",
+		user: 1,
+		content: "부적3의내용입니다. 내용입니다.",
+		image: 3,
+		total_cheer: 20,
+		cur_cheer: 20,
+		is_created: true,
+		created_at: "2023-01-13T10:34:01.382686+09:00",
+		deleted_at: null,
+		cheer: [
+			{
+				id: 1,
+				charm: 3,
+				nickname: "익명(부적3)",
+				content: "부적3의응원입니다1.",
+			},
+		],
+	},
+];
