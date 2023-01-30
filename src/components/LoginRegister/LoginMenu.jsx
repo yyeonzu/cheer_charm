f26b3f745a4d2d4b962f74fc986c666f879b1f49
yyeonzu <tooma22@naver.com>
2023-01-31 00:27:40 +0900

    fix: 카카오 로그인 이미지 깨짐 해결

--- src/components/LoginRegister/LoginMenu.jsx
+++ src/components/LoginRegister/LoginMenu.jsx
@@ -9,35 +9,16 @@ import Background from "../common/Background";
 // import Images
 import idIcon from "../../assets/images/Login/idicon.png";
 import pwIcon from "../../assets/images/Login/pwicon.png";
-import kakao from "../../assets/images/Login/kakao1.png";
+import kakao from "../../assets/images/Login/kakao2.svg";
 
 // import api
 import { RequestLogin } from "../../api/user";
-import { RequestGetUser } from "../../api/user";
-
-/*
-  JWT Token을 사용한 사용자 인증절차
-
-  프론트에서 ID와 비밀번호 전달
-  백에서 AccessToken, RefreshToken, AccessToken의 만료시간 반환
-  반환받은 AccessToken을 매 api 호출마다 헤더에 붙여서 전송
-  백엔드에서 AccessToken 체크
-  프론트에서 AccessToken의 만료가 지났다면, RefreshToken을 붙여서 백에 Reissue 요청
-  백에서 다시 AccessToken 반환
-  프론트에서 AccessToken과 만료시간 저장하여 다시 AccessToken으로 사용
-*/
-
-/* API가 아직 안나왔을 때 프론트엔드에서 TEST 하는 법
-  -Promise 사용
-  -Fake API 사용
-*/
 
 const LoginMenu = () => {
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
 
   const REST_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
-  console.log("콘솔", REST_API_KEY);
   const REDIRECT_URI = "https://cheer-charm.vercel.app/oauth";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
@@ -54,17 +35,14 @@ const LoginMenu = () => {
       return alert("비밀번호를 입력해주세요");
     } else {
       RequestLogin(id, pw)
-        .then(response => {
-          console.log(response);
-          // navigate("/");
-        })
+        .then()
         .catch(error => {
-          console.log("콘 솔", error);
+          if (error.response.status === 400) {
+            alert("로그인 실패.\n 다시 시도해주세요.");
+            setId("");
+            setPw("");
+          }
         });
-      // if (token) navigate("/");
-      // else {
-      //   alert("다시 로그인");
-      // }
     }
   };
 
