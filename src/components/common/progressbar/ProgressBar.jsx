597fd5191dc00216891c9df787f495dfea03a1d9
yyeonzu <tooma22@naver.com>
2023-02-05 20:51:21 +0900

    feat: 스크롤 보이는 것 전부 삭제

--- src/components/common/progressbar/ProgressBar.jsx
+++ src/components/common/progressbar/ProgressBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import * as S from "./ProgressBar.style";
 import clovericon from "../../../assets/images/Landing/clover.svg";
 
